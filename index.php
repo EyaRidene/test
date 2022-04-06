62bbcf59673dd982b0ebde26b8fb34d78eb0d1cd
eya ridene <eya.ridene@insat.u-carthage.tn>
2022-04-06 23:58:23 +0200

    php

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php $name='eya';
+for($i=0;$i<5;$i++)
+{
+    $ism="eya $i";
+    echo "$ism <br>";
+}
+echo 'bonjour '.$name.'<br>';
+/*$globalVar1 = 1;
+$globalVar2 = 2;
+function somme() {
+    global $globalVar1, $globalVar2;
+    $globalVar2 = $globalVar1 + $globalVar2;
+}
+somme();
+echo $globalVar2;*/
+$globalVar1 = 1;
+$globalVar2 = 2;
+function somme() {
+    $GLOBALS['globalVar2'] =
+        $GLOBALS['globalVar1'] +
+        $GLOBALS['globalVar2'];
+}
+somme();
+echo $globalVar2.'<br>';
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+bonjour <?php echo $name ?>
+</body>
+</html>
+
---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+$students= [
+    'eya'=> 'ridene',
+    'mariem'=>'ksontini'
+];
+
+foreach ($students as $value )
+{
+    echo "bienvenu $value <br>";
+}
